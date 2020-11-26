2128f35759d1c9e1992515bf19762a2c447fdb91
minhyeon <parkmh90@kr.ibm.com>
2020-11-27 00:03:18 +0900

    go ..

--- src/k8sClient/k8s.go
+++ src/k8sClient/k8s.go
@@ -1,20 +1,32 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "k8s.io/apimachinery/pkg/apis/meta/v1"
-  "k8s.io/client-go/kubernetes"
-  "k8s.io/client-go/tools/clientcmd"
+	"context"
+	"fmt"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
+func create(x int64) *int64 {
+	return &x
+}
 func main() {
-  // kubeconfig에서 현재 콘텍스트를 사용한다
-  // path-to-kubeconfig -- 예를 들어, /root/.kube/config
-  config, _ := clientcmd.BuildConfigFromFlags("", "<path-to-kubeconfig>")
-  // clientset을 생성한다
-  clientset, _ := kubernetes.NewForConfig(config)
-  // 파드를 나열하기 위해 API에 접근한다
-  pods, _ := clientset.CoreV1().Pods("").List(context.TODO(), v1.ListOptions{})
-  fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	// todo
+	// 1. get imput user's kubeconfig
+	// 2. if no input automatically use user's home/.kube/config
+	config, _ := clientcmd.BuildConfigFromFlags("https://hmsptest01:6443", "/Users/parkmh90/.kube/config")
+	clientset, _ := kubernetes.NewForConfig(config)
+
+	timeoutTime := create(10)
+	// 파드를 나열하기 위해 API에 접근한다
+	deployments, _ := clientset.AppsV1().Deployments("hmsp").List(context.TODO(), v1.ListOptions{})
+	pods, _ := clientset.CoreV1().Pods("hmsp").List(context.TODO(), v1.ListOptions{TimeoutSeconds: timeoutTime})
+	fmt.Printf("There are %d deployments in the cluster\n", len(deployments.Items))
+	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	for dep, name := range deployments.Items {
+
+	}
+
 }
